4a355279661e17147566c99387d57859e532d59e
nikorr0 <nikita.almazniy@yandex.ru>
2024-01-05 23:58:53 +0500

    Application update
    After you press button "Play" your username will be checked for special symbols and length. Then the game will be open.

--- Client/src/main/java/dev/xelond/client/Controller.java
+++ Client/src/main/java/dev/xelond/client/Controller.java
@@ -1,14 +1,64 @@
 package dev.xelond.client;
 
+import dev.xelond.client.game.RunGame;
+
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Controller {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button playButton;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    void initialize() {
+        playButton.setOnAction(actionEvent -> {
+            String username = usernameField.getText();
+            String[] symbols = ("[@-!#$%^&*()<>?/\\|}{~.,:;+'`]" + '"').split("");
+            boolean correct_username = true;
+            for (String symbol : symbols) {
+                if (username.contains(symbol)) {
+                    correct_username = false;
+                    break;
+                }
+            }
+            if (username.length() > 3 && username.length() < 16 && correct_username) {
+                System.out.println("Launching game...");
+                RunGame.launch(username);
+                // javafx.application.Platform.exit();
+            }
+            else {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Ошибка");
+
+                String error_message = "Неверное имя пользователя. ";
+                if (correct_username) {
+                    error_message += "Имя пользователя должно быть от 4 до 15 символов!";
+                }
+                else {
+                    error_message += "Имя пользователя должно содержать только цифры латинские буквы и знак \"_\"!";
+                }
+
+                alert.setHeaderText(error_message);
+                alert.showAndWait();
+            }
+
+        });
+
     }
+
 }
