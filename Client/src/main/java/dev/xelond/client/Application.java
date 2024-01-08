5bd84cb3895019c5840e317e3b8e5ac17452868f
nikorr0 <nikita.almazniy@yandex.ru>
2024-01-08 18:46:48 +0500

    Application update

--- Client/src/main/java/dev/xelond/client/Application.java
+++ Client/src/main/java/dev/xelond/client/Application.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("dev-layout.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("runtime/dev-layout.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Launcher");
         stage.setScene(scene);
