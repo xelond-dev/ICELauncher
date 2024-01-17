57caeff9fe7c2e2b9ec6f0633cea37eab55360ef
xelond-dev <xelond@proton.me>
2024-01-17 12:30:05 +0300

    Update HTTP server

--- Server/src/main/java/dev/xelond/server/HTTPServer/HTTPServer.java
+++ Server/src/main/java/dev/xelond/server/HTTPServer/HTTPServer.java
@@ -1,54 +1,29 @@
 package dev.xelond.server.HTTPServer;
 
-import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import dev.xelond.server.Config.Config;
 import dev.xelond.server.Debug;
-import dev.xelond.server.Main;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.BindException;
 import java.net.InetSocketAddress;
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
-// ? Я перпеишу этот класс..
 public class HTTPServer implements Runnable {
+
     private static HttpServer server;
-    private static final Map<String, String> pathToFileMap = new HashMap<>();
 
     @Override
     public void run() {
         try {
             server = HttpServer.create(new InetSocketAddress(Config.HostName, Config.HostPort), 0);
-
-            // ! Нужно проверить
-            /*
-            // ? В настройках необходимо добавить варианты настроек сертификатов
-            // ? Генерировать самоподписанный сертификат SSL / Использовать свой SSL / Игнорировать предупреждения
-
-            // Create SSL context
-            SSLContext sslContext = SSLContext.getInstance("TLS");
-
-            // ! Игнорирование предупреждение SSL не рекомендуется
-            // Игнорирование SSL предупреждений
-            sslContext.init(null, new TrustManager[] { new X509TrustManager() {
-                public void checkClientTrusted(X509Certificate[] chain, String authType) {}
-                public void checkServerTrusted(X509Certificate[] chain, String authType) {}
-                public X509Certificate[] getAcceptedIssuers() { return null; }
-            }}, null);
-
-            server.setHttpsMain.configurator(new HttpsMain.configurator(sslContext));
-             */
-
-            // Add server paths and file paths to the map
-            addDirectory(System.getProperty("user.home") + "/Documents/ICELauncher/shared/"); // * e.x.
-            //HttpFiles.addToShare("clients\\"); // * It is local
-
-            pathToFileMap.forEach((serverPath, filePath) -> server.createContext(serverPath, exchange -> handleRequest(exchange, filePath))); // Creating server context
+            server.createContext("/", new FileHandler());
             server.setExecutor(null); // Use the default executor
             server.start(); // Starting server
 
@@ -57,36 +32,52 @@ public class HTTPServer implements Runnable {
             Debug.basic("HTTP server " + Config.HostName + ":" + Config.HostPort + "is running already.");
         } catch (IOException e) {
             Debug.basic("Failed to start HTTP server using " + Config.HostName + ":" + Config.HostPort);
-            e.printStackTrace();
+            Debug.printStackTrace(e.toString());
         }
     }
 
-    private static void handleRequest(HttpExchange exchange, String filePath) throws IOException {
-        File file = new File(filePath);
-        if (!file.exists() || file.isDirectory()) {
-            send404NotFound(exchange);
-            return;
-        }
+    static class FileHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String requestMethod = exchange.getRequestMethod();
+            String requestUri = exchange.getRequestURI().toString();
+            String pathStr = "." + requestUri;
+            Path path = Paths.get(pathStr).normalize();
+            StringBuilder response = new StringBuilder();
 
-        // Set the response headers
-        exchange.getResponseHeaders().set("Content-Type", "application/octet-stream");
-        exchange.sendResponseHeaders(200, file.length());
+            if (Files.exists(path)) {
+                if (Files.isDirectory(path)) {
+                    List<Path> files = Files.list(path).toList();
+                    response.append("<html><body>");
+                    response.append("<h1>Files and Directories:</h1>");
+                    response.append("<ul>");
+                    for (Path file : files) {
+                        String name = file.getFileName().toString();
+                        if (Files.isDirectory(file)) {
+                            response.append("<li><a href=\"").append(name).append("/\">").append(name).append("/</a></li>");
+                        } else {
+                            response.append("<li><a href=\"").append(name).append("\">").append(name).append("</a></li>");
+                        }
+                    }
+                    response.append("</ul>");
+                    response.append("</body></html>");
+                } else {
+                    exchange.getResponseHeaders().add("Content-Disposition", "attachment; filename=\"" + path.getFileName() + "\"");
+                    exchange.sendResponseHeaders(200, Files.size(path));
+                    Files.copy(path, exchange.getResponseBody());
+                    return;
+                }
+            } else {
+                response.append("Path does not exist.");
+            }
 
-        // Write the file content to the response body
-        try (OutputStream os = exchange.getResponseBody();
-             FileInputStream fis = new FileInputStream(file)) {
-            byte[] buffer = new byte[4096];
-            int bytesRead;
-            while ((bytesRead = fis.read(buffer)) != -1) {
-                os.write(buffer, 0, bytesRead);
+            exchange.sendResponseHeaders(200, response.length());
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(response.toString().getBytes());
             }
         }
     }
 
-    private static void send404NotFound(HttpExchange exchange) throws IOException {
-        exchange.sendResponseHeaders(404, 0);
-        exchange.close();
-    }
     public void stop() {
         if (server != null) {
             server.stop(0);
@@ -94,36 +85,6 @@ public class HTTPServer implements Runnable {
             Debug.basic("HTTP server " + Config.HostName + ":" + Config.HostPort + " is stopped.");
         } else {
             Debug.basic("HTTP server is not running.");
-        }
-    }
-    public static void addDirectory(String baseDirectoryPath) {
-        Debug.log("Syncing: \"" + baseDirectoryPath + "\"");
-
-        File baseDirectory = new File(baseDirectoryPath);
-
-        listFiles(baseDirectory, baseDirectory);
-
-        // Debugging
-        for (Map.Entry<String, String> entry : pathToFileMap.entrySet()) {
-            //System.out.println("Added to share: \"" + entry.getKey() + "\"");
-            Debug.log("Sharing Debug: " + entry.getKey() + " - " + entry.getValue());
-        }
-    }
-
-    public static void listFiles(File baseDirectory, File directory) {
-        File[] files = directory.listFiles();
-        if (files != null) {
-            for (File file : files) {
-                if (file.isDirectory()) {
-                    // Рекурсивный вызов для вложенной папки
-                    listFiles(baseDirectory, file);
-                } else {
-                    // Получаем относительный путь от базовой папки
-                    String relativePath = baseDirectory.toURI().relativize(file.toURI()).getPath();
-                    // Добавляем полный путь к файлу в список
-                    pathToFileMap.put("/" + relativePath, file.getAbsolutePath());
-                }
-            }
         }
     }
 }
